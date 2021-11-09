412fd161fcf5b09f23fafab50f9c3f27c6d86d46
jrivoire <jonah.rivoire@devoteamgcloud.com>
2021-11-09 17:10:53 +0100

    Dossier pratiquement lu sauf les relations (strcture de pointeurs)

--- projet/2step.c
+++ projet/2step.c
@@ -5,7 +5,7 @@
 
 struct relation{
     char etat[10];
-    char gram[1];
+    char gram[2];
     char res[10];
 };
 
@@ -18,70 +18,94 @@ int main(int argc, char *argv[]){
       exit(EXIT_FAILURE);
    }
 
-    
-    
-    // int **Q;
-    // Q = malloc(sizeof(int)*10*10);
-    // Q[0]="hello";
-    // // printf("%s\n", Q[0]);
-    
-    // int numQ=0;
-    // char copy[0]='a';
-    
-    int X[] = {'\0'}; //Grammaire
-    int numQ=0;
-    char* copy, curr;
-    bool read = false;
-    curr = '\0';
-    copy="h";
+
+    char **X;
+    X = malloc(sizeof(char)*10*10);
+    char **Q;
+    Q = malloc(sizeof(int)*10*10);
+    char *I;
+    char **F;
+    F = malloc(sizeof(int)*10*10);
+
+    // à revoir 
+
+    // struct relation R;
+    // strcpy( R.etat, "Sara" );
+    // printf("%s\n", R.etat);
+    // struct relation **R;
+    // R = malloc(sizeof(int)*10*10);
+    // strcpy(R[0]->etat, "Sara" );
+    // printf("%s\n", R[0]->etat);
+    return 0;
     char *res;
     res = malloc(sizeof(int)*10);
 
-    strcat(res, copy);
+    char curr='\0';
+    bool read = false;
+
+    int count=0, countR=0;
 
     while((ch = fgetc(fp)) != EOF){
         if(ch=='X'){
+            count=0;
             curr = 'X';
             read=false;
         }else if(ch=='Q'){
+            count=0;
             curr = 'Q';
             read=false;
         }else if(ch=='I'){
+            count=0;
             curr = 'I';
             read=false;
         }else if(ch=='R'){
+            count=0;
             curr = 'R';
             read=false;
         }else if(ch=='F'){
+            count=0;
             curr = 'F';
             read=false;
         }
 
         if(ch==',' | ch==')' | ch=='}'){
             read=false;
-            printf("%s\n", res);
             memset(res, 0, strlen(res));
-            printf("%s\n", res);
         }
-        printf("%c", ch);
-        // printf("%s", copy);
-        // if(read==true && ch!='{'){
-        //     strcat(res,copy);
-        //     printf("%c: %s\n", copy, res);
-        //     if(curr=='X'){
-        //         strcat(X, res);
-        //         printf("%c: %s\n", curr, X);
-        //     }else if(curr=='Q'){
-        //         // Q[numQ]=*res;
-        //         // numQ++;
-        //         // printf("%c: %s\n", curr, Q);
-        //     }
-        // }
+        // printf("%c", ch);
 
-        // if(ch=='(' | ch=='{' | ch==',' | ch=='\0' | ch==' ' | ch == '='){
-        //     memset(res, 0, strlen(res));
-        //     read=true;
-        // }
+        if(read==true && ch!='{'){
+            char copy[2]={ch, '\0'};
+            strcat(res,copy);
+            if(curr=='X'){
+                X[count]=res;
+                count++;
+            }else if(curr=='Q' && strlen(res)==2){
+                Q[count]=res;
+                count++;
+            }else if(curr=='I'  && strlen(res)==2){
+                I=res;
+            }else if(curr=='F'  && strlen(res)==2){
+                F[count]=res;
+                count++;
+            }else if(curr=='R'){
+                if(res!="q" || strlen(res)==2){
+                    if(countR==0){
+
+                    }else if(countR==1){
+
+                    }else if(countR==2){
+                        countR=0;
+                    }
+                }  
+                countR++;
+            }
+        }
+
+        if(ch=='(' | ch=='{' | ch==',' | ch=='\0' | ch==' ' | ch == '='){
+            memset(res, 0, strlen(res));
+            read=true;
+        }
     }
    fclose(fp);
 }
